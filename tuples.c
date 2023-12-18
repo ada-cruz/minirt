2f3c61afefef018b73fefad957cc8381ed1bb492
Aurora Da Cruz Cardoso <ada-cruz@c1r2p4.42sp.org.br>
2023-12-18 16:37:30 -0300

    funçao de multiplicar e divisão de tuplas feito

--- tuples.c
+++ tuples.c
@@ -51,14 +51,31 @@ t_coordinates subtract_tuples(t_coordinates a, t_coordinates b)
 t_coordinates negate_tuples(t_coordinates a)
 {
 	t_coordinates new_tuple;
+
 	new_tuple.x = -1 * a.x;
 	new_tuple.y = -1 * a.y;
 	new_tuple.z = -1 * a.z;
 	new_tuple.w = -1 * a.w;
-	printf("o valor de x %f\n", new_tuple.x);
-	printf("o valor de x %f\n", new_tuple.y);
-	printf("o valor de x %f\n", new_tuple.z);
-	printf("o valor de x %f\n", new_tuple.w);
+	return (new_tuple);
+}
+
+t_coordinates multiply_tuples(double scalar, t_coordinates a)
+{
+	t_coordinates new_tuple;
+	new_tuple.x = scalar * a.x;
+	new_tuple.y = scalar * a.y;
+	new_tuple.z = scalar * a.z;
+	new_tuple.w = scalar * a.w;
+	return (new_tuple);
+}
+
+t_coordinates divide_tuples(double scalar, t_coordinates a)
+{
+	t_coordinates new_tuple;
+	new_tuple.x = a.x / scalar;
+	new_tuple.y = a.y / scalar;
+	new_tuple.z = a.z / scalar;
+	new_tuple.w = a.w / scalar;
 	return (new_tuple);
 }
 
@@ -83,7 +100,7 @@ int main(void)
 	zz = 7.0;
 	ww = 0.0;
 	
-	negate_tuples((t_coordinates){x, y, z, w});
+	divide_tuples(2.0 , (t_coordinates){x, y, z, w});
 	// if (!are_equals_tuple((t_coordinates){x, y, z, w}, (t_coordinates){xx, yy, zz, ww}))
 	// 	printf("as tuplas não são iguais");
 	// else
